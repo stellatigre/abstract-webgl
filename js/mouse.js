9ebf2b9f021cfb80fbac6b21334e0317a0e7de93
Stella Tigre <stella.cannefax@gmail.com>
2014-04-22 19:31:10 -0500

    mouse.js tidy

--- js/mouse.js
+++ js/mouse.js
@@ -1,20 +1,12 @@
 var mousePos = {"x" : 0, "y" : 0};
-
 $(document).ready(function() {
 
 $('html').mousemove(function(event){
-	//console.log("mouse move X:"+event.pageX+" Y:"+event.pageY);
 	mousePos.x = event.pageX
 	mousePos.y = event.pageY
-
-	console.log(mousePos);
+	//console.log(mousePos);
     }
 );
-
-
-
-
-
 
 });
 
