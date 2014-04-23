76fc6995af74f341daffcc930dc8238d1b5585e0
Stella Tigre <stella.cannefax@gmail.com>
2014-04-22 19:47:35 -0500

    a little bit faster now

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
 var renderer = new THREE.WebGLRenderer();
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var cubes = [];
-
-
+camera.position.z = 8;
 camera.position.z = 8;
 
 function randInt(min,max){
-	return Math.floor(Math.random() * (max-min));
+	return Math.floor(Math.random() * (max-min)+1);
 }
 
 function randomPos(shape, range) {
@@ -22,14 +21,20 @@ function randomPos(shape, range) {
 	);
 }
 
-function makeCube(size, z) {
+function moveToMouse(shape){
+	shape.applyMatrix( new THREE.Matrix4().makeTranslation(
+		mousePos.x/100, mousePos.y/100, 0
+		)
+	);	
+}
+
+function makeCube(size) {
 	
 	var randColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 	var geometry =  new THREE.CubeGeometry(size,size,size);
 	var material = new THREE.MeshBasicMaterial( { color: randColor } );
 	var cube = new THREE.Mesh( geometry, material );
-
-	console.log(cube);	
+	
 	scene.add(cube);
 	cubes.push(cube);
 	return cube
@@ -40,15 +45,28 @@ function render() {
 	cubes.forEach(function(cube, index, array) {
 		cube.rotation.x += 0.1;
 		cube.rotation.y += 0.1;
-		cube.position.x -= 0.01;
+		cube.position.x -= 0.1;
 	});
 	renderer.render(scene, camera);
 }
+
 makeCube()
-setInterval(function(){makeCube(2);}, 500);
+// big cubes
+setInterval(function(){
+	makeCube(2);
+}, 250);
+
+// medium cubes
 setInterval(function(){
 	var cube = makeCube(1); 
 	randomPos(cube, 1);
-}, 250);
+}, 100);
+
+//tiny cubes
+setInterval(function() { 
+	var cube = makeCube(0.5);	
+	moveToMouse(cube);
+}, 25);
+
 
 render();
