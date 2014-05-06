62d2f2e3d1cd83fefee0520eaa4b2d7565f769c9
Stella Tigre <stella.cannefax@gmail.com>
2014-05-06 01:21:30 -0500

    minify some shit

--- js/cube.js
+++ js/cube.js
@@ -1,10 +1,11 @@
-cubes = [];
+cubes = [];  // pos is a THREE.Vector3
 function makeCube(size, pos) {
 	
 	var randColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 	var geometry =  new THREE.CubeGeometry(size,size,size);
 	var material = new THREE.MeshBasicMaterial( { color: randColor } );
 	var cube = new THREE.Mesh( geometry, material );
+	if (pos != undefined) { cube.position = pos ; } 
 	
 	scene.add(cube);
 	cubes.push(cube);
