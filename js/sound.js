5b7176697708a9a96a67d29a4c1eab3f8252f1da
Stella Tigre <stella.cannefax@gmail.com>
2014-05-05 20:56:44 -0500

    cleanup sound.js

--- js/sound.js
+++ js/sound.js
@@ -3,6 +3,8 @@ window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var fft_co = 1;
 
+function onError(err) { console.log('fucked something up'); }
+
 function loadSound(url) {
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
@@ -24,7 +26,21 @@ function playSound(buffer, destination) {
   source.start(0);
 }
 
-var theme = ["rgba(255, 255, 255,","rgba(240, 240, 240,","rgba(210, 210, 210,","rgba(180, 180, 180,","rgba(150, 150, 150,","rgba(120, 120, 150,","rgba(90, 90, 150,","rgba(60, 60, 180,","rgba(30, 30, 180,","rgba(0, 0, 200,","rgba(0, 0, 210,","rgba(0, 0, 220,","rgba(0, 0, 230,","rgba(0, 0, 240,","rgba(0, 0, 255,","rgba(0, 30, 255,","rgba(0, 60, 255,","rgba(0, 90, 255,","rgba(0, 120, 255,","rgba(0, 150, 255,"];
+function fft2Particles(fft_co, emitters) {
+        emitters[1].speed = -5 + fft_co/3 ;              
+        emitters[1].particlesPerSecond = 20 + fft_co*36 ;
+        emitters[1].sizeStart = 2 + fft_co/2 ;              
+        emitters[1].colorStartSpread = new THREE.Vector3(5+ fft_co/20, 5-fft_co/100, 5+fft_co/6);               
+        emitters[2].particlesPerSecond = 1000 + fft_co*5 ;
+        emitters[2].sizeStart = 2 + fft_co/30 ;              
+        emitters[2].speed = 2 + fft_co/30 ;              
+        emitters[2].colorStartSpread = new THREE.Vector3(5+ fft_co/25, 5-fft_co/50, 5+fft_co/10);               
+        emitters[0].particlesPerSecond = 50 + fft_co*36 ;
+        emitters[0].speed = -4 + fft_co/3.2 ;
+        emitters[0].sizeStart = 2 + fft_co/5 ;              
+        emitters[0].radius = 28 - fft_co/30 ;
+        emitters[2].radius = 10 + fft_co/36 ;
+}
 
 var currentvalue = new Array();
 var frameBufferSize = 8192;
@@ -45,8 +61,7 @@ function audioAvailable(event) {
         outputArrayL[i] = inputArrayL[i];
         outputArrayR[i] = inputArrayR[i];
         signal[i] = (inputArrayL[i] + inputArrayR[i]) / 2;      // create data frame for fft - deinterleave and mix down to mono
-    }
-    
+    } 
     // perform forward transform
     fft.forward(signal);
     
@@ -55,24 +70,9 @@ function audioAvailable(event) {
         //if (i%1000 == 0) { console.log(magnitude); }
         currentvalue[i]= magnitude;
         fft_co = magnitude;
-        emitters = particleGroup.emitters;
-        emitters[1].speed = -5 + fft_co/3 ;              
-        emitters[1].particlesPerSecond = 20 + fft_co*36 ;
-        emitters[1].sizeStart = 2 + fft_co/2.5 ;              
-        emitters[1].colorStartSpread = new THREE.Vector3(5+ fft_co/20, 5-fft_co/100, 5+fft_co/6);               
-        emitters[2].particlesPerSecond = 1000 + fft_co*5 ;
-        emitters[2].sizeStart = 2 + fft_co/42 ;              
-        emitters[2].speed = 2 + fft_co/30 ;              
-        emitters[2].colorStartSpread = new THREE.Vector3(5+ fft_co/25, 5-fft_co/50, 5+fft_co/10);               
-        emitters[0].particlesPerSecond = 50 + fft_co*36 ;
-        emitters[0].speed = -4 + fft_co/3.2 ;
-        emitters[0].sizeStart = 2 + fft_co/8 ;              
-        emitters[0].radius = 28 - fft_co/30 ;
-        emitters[2].radius = 10 + fft_co/36 ;
+		fft2Particles(fft_co, particleGroup.emitters);
     }
 }
-
-var onError = function(err) { console.log('fucked something up'); }
 
 // begin doing stuff
 var jsProc = context.createScriptProcessor(8192);
@@ -82,5 +82,5 @@ loadSound('http://localhost:8080/audio/02%20Kickflip.mp3');
 setTimeout(function() { 
 	playSound(musicBuffer, jsProc) ; 
 	jsProc.connect(context.destination);
-} , 8500);
+} , 7500);
 console.log('STARTING...');
